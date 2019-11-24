d095652322c14d8c985a7b47a23ce43aaf0eb43b
Kagura Chen <daniyuu@163.com>
2019-11-24 15:51:43 +0800

    test

--- flow/tutorial.py
+++ flow/tutorial.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
 default_args = {
@@ -17,7 +18,7 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG('tutorial', default_args=default_args, schedule_interval=timedelta(days=1))
+dag = DAG('tutorial1', default_args=default_args, schedule_interval=timedelta(days=1))
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
@@ -25,25 +26,17 @@ t1 = BashOperator(
     bash_command='date',
     dag=dag)
 
-t2 = BashOperator(
-    task_id='sleep',
-    bash_command='sleep 5',
-    retries=3,
-    dag=dag)
 
-templated_command = """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-"""
+def print_context(ds, **kwargs):
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
 
-t3 = BashOperator(
-    task_id='templated',
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
-    dag=dag)
 
-t2.set_upstream(t1)
-t3.set_upstream(t1)
+run_this = PythonOperator(
+    task_id='print_the_context',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag,
+)
+
+run_this.set_upstream(t1)
